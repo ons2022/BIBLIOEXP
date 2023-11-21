f571ed044c0ccee64b1efb1f3a81031a29aecafa
onsiii <onsiii>
2023-11-21 14:13:22 +0100

    first commit

---
+++ src/main/java/biblioexp/bibleo/BibleoApplication.java
@@ -0,0 +1,13 @@
+package biblioexp.bibleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleoApplication.class, args);
+	}
+
+}
