62045da755d72570d5b0ec4993e934a66e67e4b6
ons2022 <onsismailhajri@gmail.com>
2023-12-27 00:40:09 +0100

    books

--- src/main/java/biblioexp/bibleo/Controller/BookController.java
+++ src/main/java/biblioexp/bibleo/Controller/BookController.java
@@ -1,13 +1,12 @@
 package biblioexp.bibleo.Controller;
 
 import biblioexp.bibleo.Entity.Book;
+import biblioexp.bibleo.Entity.User;
 import biblioexp.bibleo.Imp.BookServiceImp;
 import biblioexp.bibleo.Service.BookService;
-
-
-
-
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
@@ -19,55 +18,79 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/api/Books")
 public class BookController {
     private final BookService BookService;  // Changed the variable name to follow camelCase
+
     public BookController(BookService bookService) {
         this.BookService = bookService;
     }
+
     // build create Book REST API
     @PostMapping()
-    public ResponseEntity<Book> saveBook(@RequestBody Book Book){
+    public ResponseEntity<Book> saveBook(@RequestBody Book Book) {
         return new
                 ResponseEntity<Book>(BookService.saveBook(Book),
                 HttpStatus.CREATED);
     }
+
     // build get all Books REST API
     @GetMapping
-    public List<Book> getAllBooks(){
+    public List<Book> getAllBooks() {
         return BookService.getAllBooks();
     }
+
     // build get Book by id REST API
 // http://localhost:8080/api/Books/1
     @GetMapping("{id}")
     public ResponseEntity<Book> getBookById(@PathVariable("id")
-                                            long BookId){
+                                            long BookId) {
         return new
                 ResponseEntity<Book>(BookService.getBookById(BookId),
                 HttpStatus.OK);
     }
+
     // build update Book REST API
 // http://localhost:8080/api/Books/1
     @PutMapping("{id}")
     public ResponseEntity<Book> updateBook(@PathVariable("id")
                                            long id
-            ,@RequestBody Book Book){
+            , @RequestBody Book Book) {
         return new
                 ResponseEntity<Book>(BookService.updateBook(Book, id),
                 HttpStatus.OK);
     }
+
     // build delete Book REST API
 // http://localhost:8080/api/Books/1
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteBook(@PathVariable("id") long
-                                                     id){
+                                                     id) {
 // delete Book from DB
         BookService.deleteBook(id);
         return new ResponseEntity<String>("Book deleted successfully!.", HttpStatus.OK);
     }
 
+    @GetMapping("/BookList")
+    public ModelAndView showBookList() {
+        List<Book> BookList = BookService.getAllBooks();
+        ModelAndView modelAndView = new ModelAndView("BookList");
+        modelAndView.addObject("BookList", BookList);
+        return modelAndView;
 
+    }
+    @GetMapping("/{BookId}/edit")
+    public ModelAndView showUpdateForm(@PathVariable Long BookId) {
+        // Retrieve the user from the database based on userId
+
+
+        ModelAndView modelAndView = new ModelAndView("update-Book");
+
+
+        return modelAndView;
+    }
 
 }
